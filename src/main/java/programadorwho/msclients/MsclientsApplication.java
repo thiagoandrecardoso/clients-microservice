40563d63519c6a5a3fdd9ad6ee471d8a72c2cb92
Thiago <thiagoandrecardoso@icloud.com>
2022-07-03 16:31:22 -0300

    initial commit

---
+++ src/main/java/programadorwho/msclients/MsclientsApplication.java
@@ -0,0 +1,13 @@
+package programadorwho.msclients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsclientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsclientsApplication.class, args);
+	}
+
+}
