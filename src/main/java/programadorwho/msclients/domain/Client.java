441791e16da30bdf0e2445f97642aedcef401d46
Thiago <thiagoandrecardoso@icloud.com>
2022-07-07 20:54:41 -0300

    Added entity client and dependency starter jpa

---
+++ src/main/java/programadorwho/msclients/domain/Client.java
@@ -0,0 +1,27 @@
+package programadorwho.msclients.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String cpf;
+    @Column
+    private String nome;
+    @Column
+    private Integer idade;
+
+    public Client(String cpf, String nome, Integer idade) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
